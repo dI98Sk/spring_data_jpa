8af3930c7175408f3edbd817b8a91854a87a4e0d
Dmitrii Skakun <112588946+dI98Sk@users.noreply.github.com>
2023-06-11 11:14:02 +0300

    initial commit

---
+++ src/main/java/com/skakakundima/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.skakakundima.spring.springboot.spring_data_jpa.dao;
+
+
+
+
+import com.skakakundima.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
